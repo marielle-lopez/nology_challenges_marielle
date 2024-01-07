d4f0ac2cbb062d66b186e03cf89089358f46567d
marielle-lopez <92203731+marielle-lopez@users.noreply.github.com>
2024-01-07 16:25:14 +1100

    completed js11 challenges

--- challenges/javascript/src/js11-unit-testing/challenge.test.js
+++ challenges/javascript/src/js11-unit-testing/challenge.test.js
@@ -1,1 +1,35 @@
-import * as challenge from "./js11";
+import * as challenge from "./js11.js";
+
+describe("Testing unit tests for JS11 challenge", () => {
+  it("Should return 'Hello, nice to meet you'", () => {
+    expect(challenge.greet()).toBe("Hello, nice to meet you");
+  });
+
+  it("Should return the correct sum of the two given numbers", () => {
+    expect(challenge.sumTwoNumbers(1, 2)).toBe(3);
+  });
+
+  it("Should return the correct customised string depending on the two given numbers", () => {
+    expect(challenge.checkLarger(1, 1)).toBe("Both numbers are equal");
+    expect(challenge.checkLarger(1, 2)).toBe("2 is bigger than 1");
+    expect(challenge.checkLarger(3, 1)).toBe("3 is bigger than 1");
+  });
+
+  it("Should return a string if names in given array are longer than 6 letters, otherwise returns an array of the valid names", () => {
+    expect(
+      challenge.filterByLength(["Chihiro", "Haku", "Lin", "Zeniba", "No-Face"])
+    ).toStrictEqual(["Haku", "Lin", "Zeniba"]);
+    expect(
+      challenge.filterByLength([
+        "Spider-Man",
+        "Iron Man",
+        "Captain America",
+        "Black Widow",
+      ])
+    ).toBe("Sorry, no valid names supplied");
+  });
+
+  it("Should return an array where the numbers of the supplied array are replaced with 0 if the difference between the number and the reducer is less than 0, or replaced with 255 if the difference is larger than 255, otherwise replaced with the difference", () => {
+    expect(challenge.reduceNumbers([2, 300, 7], 5)).toStrictEqual([0, 255, 2]);
+  });
+});
